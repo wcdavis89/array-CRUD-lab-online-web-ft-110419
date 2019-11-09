e0f6907ba86d5566cfd8feb8af3c3ad8d04f9060
Whittney Davis <wcdavis89@yahoo.com>
2019-11-09 02:58:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  new_array=[]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+array[-1]  
 end
